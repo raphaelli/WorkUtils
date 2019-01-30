3809306d068639ec9aced3b6c4d372b0fc6af2c7
楠槡 <raphael_li@live.com>
2019-01-30 16:38:11 +0800

    Create getDaysInOneMonth.js

---
+++ JavaScript/getDaysInOneMonth.js
@@ -0,0 +1,9 @@
+function getDaysInOneMonth(year, month) {
+    month = parseInt(month, 10);
+    var d = new Date(year, month, 0);
+    return d.getDate();
+}
+
+//eg:
+//console.info(getDaysInOneMonth(2019, 02));
+// -- 28
